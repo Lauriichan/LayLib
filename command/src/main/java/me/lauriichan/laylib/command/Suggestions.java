b11faa2a20140be6399155eed8f338650502bd2e
Lauriichan <lauriichan@playuniverse.org>
2024-03-02 14:26:20 +0100

    Improve Suggestions class

--- command/src/main/java/me/lauriichan/laylib/command/Suggestions.java
+++ command/src/main/java/me/lauriichan/laylib/command/Suggestions.java
@@ -25,15 +25,17 @@ public final class Suggestions {
         }
         suggestion.remove(value.toString());
     }
-    
+
     public boolean hasSuggestions() {
         return !suggestion.isEmpty();
     }
 
     @SuppressWarnings("unchecked")
     public Entry<String, Double>[] getSuggestions(int depth) {
-        return suggestion.entrySet().stream().sorted(SuggestionSorter.INSTANCE).limit(Math.max(depth, 1))
-            .toArray(Entry[]::new);
+        if (depth <= 0) {
+            return suggestion.entrySet().stream().sorted(SuggestionSorter.INSTANCE).toArray(Entry[]::new);
+        }
+        return suggestion.entrySet().stream().sorted(SuggestionSorter.INSTANCE).limit(depth).toArray(Entry[]::new);
     }
 
     private static final class SuggestionSorter implements Comparator<Entry<String, Double>> {
