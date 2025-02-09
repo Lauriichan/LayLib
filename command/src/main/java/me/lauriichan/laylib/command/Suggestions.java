36d2bcea83429245a236481b6666488d57b8a373
Lauriichan <lauriichan@playuniverse.org>
2025-02-09 14:38:10 +0100

    Improve Suggestions#getSuggestions

--- command/src/main/java/me/lauriichan/laylib/command/Suggestions.java
+++ command/src/main/java/me/lauriichan/laylib/command/Suggestions.java
@@ -1,10 +1,16 @@
 package me.lauriichan.laylib.command;
 
+import java.lang.invoke.MethodHandle;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
+import me.lauriichan.laylib.reflection.JavaLookup;
+
 public final class Suggestions {
+
+    private static final MethodHandle ELEMENT_DATA_ACCESS = JavaLookup.PLATFORM.findGetter(ArrayList.class, "elementData", Object[].class);
 
     private final ConcurrentHashMap<String, Double> suggestion = new ConcurrentHashMap<>();
 
@@ -32,10 +38,20 @@ public final class Suggestions {
 
     @SuppressWarnings("unchecked")
     public Entry<String, Double>[] getSuggestions(int depth) {
+        ArrayList<Entry<String, Double>> list = new ArrayList<>(suggestion.entrySet());
+        list.sort(SuggestionSorter.INSTANCE);
         if (depth <= 0) {
-            return suggestion.entrySet().stream().sorted(SuggestionSorter.INSTANCE).toArray(Entry[]::new);
+            return list.toArray(Entry[]::new);
         }
-        return suggestion.entrySet().stream().sorted(SuggestionSorter.INSTANCE).limit(depth).toArray(Entry[]::new);
+        Entry<String, Double>[] output = new Entry[Math.min(list.size(), depth)];
+        try {
+            System.arraycopy((Object[]) ELEMENT_DATA_ACCESS.invoke(list), 0, output, 0, output.length);
+        } catch (Throwable e) {
+            for (int i = 0; i < output.length; i++) {
+                output[i] = list.get(i);
+            }
+        }
+        return output;
     }
 
     private static final class SuggestionSorter implements Comparator<Entry<String, Double>> {
